f1d7182a90091be1e40c28a109034304d7fd43ba
DollarBusiness <dollarbusiness91@gmail.com>
2017-02-13 20:21:01 -0600

    Update ComputeExpressions.java

--- ComputeExpressions.java
+++ ComputeExpressions.java
@@ -4,7 +4,7 @@ public class ComputeExpressions {
 
     public static void main(String[] args) {
         System.out.println("Math problem: 9.5 * 4.5 - 2.5 * 3 / 45.5 - 3.5");
-        System.out.print("Answer: 0.83928571");
+        System.out.println("Answer: 0.83928571");
     }
     
 }
