5c50fb4b62cd3c878e6d0e3fd08eeb7556c9e45c
DollarBusiness <dollarbusiness91@gmail.com>
2017-02-13 20:19:32 -0600

    Add files via upload

---
+++ ComputeExpressions.java
@@ -0,0 +1,10 @@
+package computeexpressions;
+
+public class ComputeExpressions {
+
+    public static void main(String[] args) {
+        System.out.println("Math problem: 9.5 * 4.5 - 2.5 * 3 / 45.5 - 3.5");
+        System.out.print("Answer: 0.83928571");
+    }
+    
+}
